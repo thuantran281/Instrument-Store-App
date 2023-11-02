bb3f3269cd1b60b4c680bee9a295a951862099ac
Thuan Tran <tranthuan12002@gmail.com>
2023-11-03 00:44:21 +0200

    adding multi-carousel for testing

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,20 +4,45 @@ import NavBar from "../../Components/Navbar";
 import Carousels from "../../Components/Carousels";
 import Footer from "../../Components/Footer";
 import ScrollToTop from "../../Components/ScrollToTop";
-import { Image } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const Responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 4, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 3,
+    slidesToSlide: 3, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 464 },
+    items: 2,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 
 const ProductSliderUrl = [
   {
-    url: "",
+    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
   },
   {
-    url: "",
+    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
   },
   {
-    url: "",
+    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
   },
   {
-    url: "",
+    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
+  },
+  {
+    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
+  },
+  {
+    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
   },
 ];
 
@@ -30,8 +55,25 @@ const Home = () => {
       <div className="my-xl-4"></div>
       <h1 className="product-title">Products</h1>
       <ScrollToTop />
-      <div>
-        <h4>Guitar</h4>
+      <div className="parent">
+        <Carousel
+          responsive={Responsive}
+          autoPlay={true}
+          swipeable={true}
+          draggable={true}
+          showDots={true}
+          infinite={true}
+          partialVisible={false}
+          dotListClass="custom-dot-list-style"
+        >
+          {ProductSliderUrl.map((imageUrl, index) => {
+            return (
+              <div className="slider" key={index}>
+                <img src={imageUrl.url} alt="guitar" />
+              </div>
+            );
+          })}
+        </Carousel>
       </div>
       <Footer />
     </>
