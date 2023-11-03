6773f2b6ea80ae6b1aedc3954606e649d61dd12a
Thuan Tran <tranthuan12002@gmail.com>
2023-11-04 01:12:19 +0200

    image url fixed custom css for carousel added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,43 +6,44 @@ import Footer from "../../Components/Footer";
 import ScrollToTop from "../../Components/ScrollToTop";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import Carousel1 from "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg";
 
-const Responsive = {
+const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 4,
-    slidesToSlide: 4, // optional, default to 1.
+    slidesToSlide: 4, 
   },
   tablet: {
     breakpoint: { max: 1024, min: 768 },
     items: 3,
-    slidesToSlide: 3, // optional, default to 1.
+    slidesToSlide: 3,
   },
   mobile: {
     breakpoint: { max: 767, min: 464 },
     items: 2,
-    slidesToSlide: 1, // optional, default to 1.
+    slidesToSlide: 1,
   },
 };
 
 const ProductSliderUrl = [
   {
-    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
+    url: Carousel1,
   },
   {
-    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
+    url: Carousel1,
   },
   {
-    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
+    url: Carousel1,
   },
   {
-    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
+    url: Carousel1,
   },
   {
-    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
+    url: Carousel1,
   },
   {
-    url: "../../Assets/Images/Guitar Products/2019-12-22-00_540.cef335a3cc4d2fe270b9.jpg",
+    url: Carousel1,
   },
 ];
 
@@ -55,9 +56,10 @@ const Home = () => {
       <div className="my-xl-4"></div>
       <h1 className="product-title">Products</h1>
       <ScrollToTop />
-      <div className="parent">
+      <div>
+        <h3 className="mx-xl-4">Guitar</h3>
         <Carousel
-          responsive={Responsive}
+          responsive={responsive}
           autoPlay={true}
           swipeable={true}
           draggable={true}
