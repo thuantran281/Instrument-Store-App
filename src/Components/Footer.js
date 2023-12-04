77f590c692c0492f0a939b262d3e4cb328ae3be7
Thuan Tran <tranthuan12002@gmail.com>
2023-12-05 00:20:37 +0200

    styling Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,26 @@
 import React from "react";
+// import guitarIcon from "../Assets/Images/guitar-logo.png";
+import "./Footer.css"
 
 const Footer = () => {
   return (
     <>
-      <h4>Here goes the Footer section</h4>
+      <footer className="footer">
+        <div className="footer-content">
+          <div className="footer-section">
+            <h3>Section 1</h3>
+            <p>Content for section 1 goes here.</p>
+          </div>
+          <div className="footer-section">
+            <h3>Section 2</h3>
+            <p>Content for section 2 goes here.</p>
+          </div>
+          <div className="footer-section">
+            <h3>Section 3</h3>
+            <p>Content for section 3 goes here.</p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
