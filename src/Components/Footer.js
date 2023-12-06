681b6773b3cbe1765889edd4b721b165c1fdf67a
Thuan Tran <tranthuan12002@gmail.com>
2023-12-07 01:07:05 +0200

    footer colour added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
-// import guitarIcon from "../Assets/Images/guitar-logo.png";
-import "./Footer.css"
+import guitarIcon from "../Assets/Images/guitar-logo.png";
+import { BsFacebook, BsInstagram, BsTiktok } from "react-icons/bs";
+import "./Footer.css";
 
 const Footer = () => {
   return (
@@ -8,11 +9,11 @@ const Footer = () => {
       <footer className="footer">
         <div className="footer-content">
           <div className="footer-section">
-            <h3>Section 1</h3>
+            <h3>About us</h3>
             <p>Content for section 1 goes here.</p>
           </div>
           <div className="footer-section">
-            <h3>Section 2</h3>
+            <h3>Opening Hours</h3>
             <p>Content for section 2 goes here.</p>
           </div>
           <div className="footer-section">
