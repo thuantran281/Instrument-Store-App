41bab8f84ddb27aad0afd3f839f772b770d766bb
Thuan Tran <tranthuan12002@gmail.com>
2023-12-12 00:30:22 +0200

    adding path and styling footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import About from "./Pages/About/About";
 import Blogs from "./Pages/Blogs/Blogs";
 import Contact from "./Pages/Contact/Contact";
 import News from "./Pages/News/News";
+import Guitar from "./Pages/Products/Guitar/Guitar";
+import Organ from "./Pages/Products/Organ/Organ";
+import Drum from "./Pages/Products/Drum/Drum";
+import Piano from "./Pages/Products/Piano/Piano";
+import Ukulele from "./Pages/Products/Ukulele/Ukulele";
+import Payment from "./Pages/Payment/Payment";
 
 const App = () => {
   return (
@@ -12,6 +18,12 @@ const App = () => {
       <Routes>
         <Route>
           <Route index element={<Home />} />
+          <Route path="/products/guitar" exact Component={Guitar}/>
+          <Route path="/products/piano" Component={Piano}/>
+          <Route path="/products/drum" Component={Drum}/>
+          <Route path="/products/organ" Component={Organ}/>
+          <Route path="/products/ukulele" Component={Ukulele}/>
+          <Route path="/payment" exact Component={Payment}/>
           <Route path="/blog" element={<Blogs />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,15 @@
 import React from "react";
-import guitarIcon from "../Assets/Images/guitar-logo.png";
-import { BsFacebook, BsInstagram, BsTiktok } from "react-icons/bs";
+// import guitarIcon from "../Assets/Images/guitar-logo.png";
+import {
+  BsTelephone,
+  BsEnvelope,
+  BsFacebook,
+  BsInstagram,
+  BsTiktok,
+  BsFillPinMapFill,
+} from "react-icons/bs";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -9,15 +17,78 @@ const Footer = () => {
       <footer className="footer">
         <div className="footer-content">
           <div className="footer-section">
-            <h3>About us</h3>
-            <p>Content for section 1 goes here.</p>
+            <h3>
+              <Link
+                to="/about"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                About us
+              </Link>
+            </h3>
+            <div>
+              <a
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <BsFacebook style={{ fontSize: "20px" }} />
+              </a>
+              &nbsp;
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <BsInstagram style={{ fontSize: "20px", color: "#e4405f" }} />
+              </a>
+              &nbsp;
+              <a
+                href="https://www.tiktok.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <BsTiktok style={{ fontSize: "20px", color: "black" }} />
+              </a>
+            </div>
+            <div className="my-xl-3">
+              <a
+                href="mailto: abc123@hotmail.com"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <BsEnvelope />
+                &nbsp; abc123@hotmail.com <br />
+              </a>
+              &nbsp;
+              <a
+                href="tel: +12345678901"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                &nbsp;
+                <BsTelephone /> +12345678901 <br />
+              </a>
+              <div>
+                <BsFillPinMapFill />
+                &nbsp; Vidnasinkatu 2, 67100 Kokkola
+              </div>
+            </div>
           </div>
           <div className="footer-section">
             <h3>Opening Hours</h3>
-            <p>Content for section 2 goes here.</p>
+            <ul className="opening-hours-list">
+              <li>
+                <span>Monday:</span> 9:00 AM - 6:00 PM
+              </li>
+              <li>
+                <span>Tuesday:</span> 9:00 AM - 6:00 PM
+              </li>
+              <li>
+                <span>Wednesday:</span> 9:00 AM - 6:00 PM
+              </li>
+              {/* Add other days as needed */}
+            </ul>
           </div>
           <div className="footer-section">
-            <h3>Section 3</h3>
+            <h3>Find us</h3>
             <p>Content for section 3 goes here.</p>
           </div>
         </div>
