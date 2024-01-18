c05ae04d7bf71fbbc6e6f5b961c93d837b7027fd
Thuan Tran <tranthuan12002@gmail.com>
2024-01-19 01:48:23 +0200

    adding url redirect

--- src/Pages/News/News.js
+++ src/Pages/News/News.js
@@ -5,6 +5,10 @@ import Footer from "../../Components/Footer";
 import axios from "axios";
 import "./News.css";
 
+const redirectUrl = (url) => {
+  window.open(url, "_blank");
+};
+
 const News = () => {
   const [news, setNews] = useState([]);
   const apiKey = "3e74e29cbe794f04910adbb1199319f4";
@@ -32,10 +36,22 @@ const News = () => {
       <div className="news-container">
         {news.map((article) => (
           <div key={article.url} className="news-card card-width">
-            <img src={article.urlToImage} alt={article.title} className="card-image" />
+            <img
+              src={article.urlToImage}
+              alt={article.title}
+              className="card-image"
+            />
             <div className="card-content">
               <h3 className="card-title">{article.title}</h3>
               <p className="card-description">{article.description}</p>
+              <a
+                href={article.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={() => redirectUrl(article.url)}
+              >
+                Read More
+              </a>
             </div>
           </div>
         ))}
