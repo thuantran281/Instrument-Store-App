4132eb2abb32fdcffcb1413c2128d58120687a58
Thuan Tran <tranthuan12002@gmail.com>
2024-03-11 20:40:08 +0200

    header modified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,50 +1,42 @@
 import React from "react";
 import guitarIcon from "../Assets/Images/guitar-logo.png";
-import { Row, Col } from "react-bootstrap";
-import { FaCartShopping } from "react-icons/fa6";
-import { BsSearch } from "react-icons/bs";
+import { BsSearch, BsCart } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <Row className="d-flex justify-content-between align-items-center">
-        <Col className="text-center guitar-logo">
+      <div className="row d-flex justify-content-between align-items-center">
+        <div className="col text-center guitar-logo">
           <Link to="/">
             <img
-              className="guitar-logo"
+              className="guitar-logo img-fluid"
               src={guitarIcon}
               alt="guitar"
-              fluid="true"
             />
           </Link>
-        </Col>
-        <Col
-          className="mx-4 mx-sm-4 mx-md-4 mx-lg-4 mx-xl-4 my-xl-4"
-          xs="auto"
-          xl="auto"
-          md="auto"
-          sm="auto"
-        >
-          <Link
-            to="/shopping-cart"
-            style={{ textDecoration: "none", color: "black" }}
-          >
-            <FaCartShopping className="icon" />
+        </div>
+        <div className="col-auto col-sm-auto col-md-auto col-lg-auto col-xl-auto">
+          <i className="icon">
+            <BsSearch />
+          </i>
+          <Link to="/shopping-cart" className="text-decoration-none text-dark">
+            <i className="icon">
+              <BsCart />
+            </i>
           </Link>
-          <BsSearch className="icon" />
-        </Col>
-      </Row>
-      <Row>
-        <Col
-          className="text-center brand_name"
+        </div>
+      </div>
+      <div className="row">
+        <div
+          className="col text-center brand_name"
           style={{ fontSize: "38px", marginLeft: "10px" }}
         >
-          <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+          <Link to="/" className="text-decoration-none text-dark">
             Instrumental Store
           </Link>
-        </Col>
-      </Row>
+        </div>
+      </div>
     </div>
   );
 };
